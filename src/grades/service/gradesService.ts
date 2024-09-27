bc9b0c4f66a0d575c12dd39e05998003b8c12dec
Aniol Rodriguez <arodrigue96@gmail.com>
2024-09-27 13:37:35 +0200

    Create addGrade() to add new grades into the array and check if grade already exists

--- src/grades/service/gradesService.ts
+++ src/grades/service/gradesService.ts
@@ -20,4 +20,46 @@ export const getGradesTotal = (grades: Grade[]): number => grades.length;
 // Crea una función para crear una nueva nota
 // La función debe recibir un array de notas, el id del estudiante, el id del curso y el valor de la nota
 // Si la nota ya existe, muestra un error con showErrorModal
-// export const addGrade =
+
+export const addGrade = (
+  grades: Grade[],
+  studentId: number,
+  courseId: number,
+  gradeValue: number
+): void => {
+  const gradeData = {
+    id: generateId(courses),
+    studentId: studentId,
+    courseId: courseId,
+    value: gradeValue,
+  };
+
+  const gradeExist = grades.some(
+    (grade) => grade.courseId === gradeData.id || grade.value === gradeValue
+  );
+
+  if (!gradeExist) {
+    grades.push(gradeData);
+  } else {
+    showErrorModal("La nota para este curso ya existe");
+  }
+};
+
+/*const studentData: Student = {
+    id: generateId(students),
+    name: studentName,
+    lastName: studentLastName,
+    age: studentAge,
+    email: studentEmail,
+    phoneNumber: studentPhoneNumber,
+  };
+  const isStudentInList = students.some(
+    (student) => student.email === studentEmail
+  );
+
+  if (!isStudentInList) {
+    students.push(studentData);
+  } else {
+    showErrorModal("El alumno ya existe");
+  }
+};*/
